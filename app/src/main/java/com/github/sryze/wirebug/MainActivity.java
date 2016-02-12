d8c77b6f24d6d982e7d8608561aaab85da10916d
Sergey Zolotarev <sryze@yandex.com>
2016-02-12 21:44:14 +0600

    Delete garbage

--- app/src/main/java/com/github/sryze/wirebug/MainActivity.java
+++ app/src/main/java/com/github/sryze/wirebug/MainActivity.java
@@ -21,7 +21,6 @@ import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
-import android.net.ConnectivityManager;
 import android.net.wifi.WifiInfo;
 import android.net.wifi.WifiManager;
 import android.os.Bundle;
